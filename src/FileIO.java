77e15f5c9c3daa0e554ab3200ac87bd628368db5
ChoboAvenger <fakeyourlife@hotmail.com>
2018-11-26 20:51:45 -0500

    commit this file omg omg omg

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,3 +1,50 @@
+//Mark Sollazzo
+//260873844
+
+import java.io.*;
+
 class FileIO
 {
+    
+    public static Character readCharacter(String filename)
+    {
+        
+        
+        FileReader fr = new FileReader(filename);
+        BufferedReader br = new BufferedReader(fr);
+        String currentLine = br.readLine();
+        while(currentLine != null)
+        {
+            System.out.println(currentLine);
+            currentLine = br.readLine();
+        }
+        try
+        {
+            System.out.println(currentLine);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Invalid name, please try again.");
+        }
+        catch (java.io.FileNotFoundException f)
+        {
+            System.out.println("File nout found.");
+        }
+        br.close();
+        fr.close();
+    }
 }
+
+
+
+    
+    
+   /*try {
+            writeToFile(w, "fruits.txt");
+            writeToFile(f, "fruits.txt");
+        } catch (IOException e) {
+            System.out.println("something went wrong");
+    */
+    
+    
+
