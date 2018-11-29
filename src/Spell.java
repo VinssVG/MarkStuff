00b3fa1bffee652c23cd952e881e365d740d4e3a
ChoboAvenger <fakeyourlife@hotmail.com>
2018-11-29 17:48:15 -0500

    Has stuff in it now

--- src/Spell.java
+++ src/Spell.java
@@ -3,4 +3,32 @@
 
 class Spell
 {
+    private final String name;
+    private final double minDamage;
+    private final double maxDamage;
+    private final double accuracy;
+    
+    public static Spell(String name, double minDamage, double maxDamage, double accuracy)
+    {
+        this.name = name;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+        this.accuracy = accuracy;
+    }
+        
+    public static String getName()
+    {
+        return name;
+    }
+    
+    public static getMagicDamage()
+    {
+        
+    }
+    
+    public static toString()
+    {
+        return this.name + "'s does " +minDamage+ "-"+maxDamage+" and has an accuracy of " +accuracy+ ".";
+       
+    }
 }
